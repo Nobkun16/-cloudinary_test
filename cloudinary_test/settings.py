d41bf3474ccd46348e474e1b96efa187acb6a4c8
John Philip Baylon <johnphilipbaylon17@gmail.com>
2025-04-26 18:58:16 +0800

    first_commit

--- cloudinary_test/settings.py
+++ cloudinary_test/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!m_*5t!3n4mt3qn=d16)dj6z6=jnju$0-oxqsgcnvshy2ssa%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
